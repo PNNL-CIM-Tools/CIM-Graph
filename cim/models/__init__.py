f16f4358dee6ec0da8b139c3445d311ff447c20a
C. Allwardt <3979063+craig8@users.noreply.github.com>
2022-08-23 13:34:10 -0700

    add init

---
+++ cim/models/__init__.py
@@ -0,0 +1,5 @@
+# from . distributed_model import DistributedModel
+# from . switch_area import SwitchArea
+# from . secondary_area import SecondaryArea
+#
+# __all__ = ['DistributedModel', 'SwitchArea', 'SecondaryArea']
