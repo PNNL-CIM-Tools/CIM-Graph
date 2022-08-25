a05b74317e6a75ee98fabd3435a98c346ca1bd99
AAndersn <alexander.a.anderson@ieee.org>
2022-08-25 00:48:34 +0000

    SPARQL structure

--- loaders/__init__.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-from terminal_loader import load_terminals_by_conducting_equipment
-
-__all__: List[str] = [
-    "load_terminals_by_conducting_equipment"
-]
--- loaders/terminal_loader.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from typing import List
-
-from gridappsd_cim import Terminal, ConductingEquipment
-
-
-def load_terminals_by_conducting_equipment(conducting: str | ConductingEquipment) -> List[Terminal]:
-    return [Terminal(mRID="foo")]
