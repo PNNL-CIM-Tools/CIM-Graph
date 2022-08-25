2e05988364fe9a906e5148fdb1e543794cba749f
AAndersn <alexander.a.anderson@ieee.org>
2022-08-25 17:52:25 +0000

    SPARQL structure

--- cim/loaders/sparql/__init__.py
+++ cim/loaders/sparql/__init__.py
@@ -1,4 +1,18 @@
+from cim.loaders.sparql.ac_dc_terminal import ACDCTerminalSPARQL
 from cim.loaders.sparql.ac_line_segment import ACLineSegmentSPARQL
+from cim.loaders.sparql.ac_line_segment_phase import ACLineSegmentPhaseSPARQL
+from cim.loaders.sparql.asset import AssetSPARQL
+from cim.loaders.sparql.asset_container import AssetContainerSPARQL
+from cim.loaders.sparql.asset_info import AssetInfoSPARQL
+from cim.loaders.sparql.base_voltage import BaseVoltageSPARQL
+from cim.loaders.sparql.breaker import BreakerSPARQL
+# from cim.loaders.sparql.bus_bar_section import BusBarSectionSPARQL
+# from cim.loaders.sparql.bus_bar_section_info import BusBarSectionInfoSPARQL
+from cim.loaders.sparql.cable_info import CableInfoSPARQL
+
+from cim.loaders.sparql.energy_consumer import EnergyConsumerSPARQL
+
+
 from cim.loaders.sparql.linear_shunt_compensator import LinearShuntCompensatorSPARQL
 
 
