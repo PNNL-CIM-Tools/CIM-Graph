352f45b6e36dfe327a3119bf4d5a8cb2de760e95
Alexander Anderson <alexander.a.anderson@ieee.org>
2023-04-04 09:31:41 -0700

    renamed root to cimgraph

--- cimgraph/loaders/gridappsd/__init__.py
+++ cimgraph/loaders/gridappsd/__init__.py
@@ -23,8 +23,8 @@ sparql = None
 def set_cim_profile(cim_profile: CIM_PROFILE):
     global cim
     global sparql
-    cim = importlib.import_module('cimlab.data_profile.' + cim_profile)
-    sparql = importlib.import_module('cimlab.loaders.sparql.' + cim_profile)
+    cim = importlib.import_module('cimgraph.data_profile.' + cim_profile)
+    sparql = importlib.import_module('cimgraph.loaders.sparql.' + cim_profile)
 
 os.environ["GRIDAPPSD_ADDRESS"] = "localhost"
 os.environ["GRIDAPPSD_PORT"] = "61613"
