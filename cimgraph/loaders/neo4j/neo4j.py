2cfd9ced3c8341e3054e13040741d164842977ea
aandersn <alexander.a.anderson@ieee.org>
2023-08-08 21:22:21 -0700

    workaround for missing mrid

---
+++ cimgraph/loaders/neo4j/neo4j.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+import math
+import importlib
+import logging
+import re
+from typing import Dict, List, Optional
+
+from cimgraph.loaders import ConnectionInterface, ConnectionParameters, Parameter, QueryResponse
+from cimgraph.models.model_parsers import add_to_graph, add_to_catalog, item_dump
+from neo4j import GraphDatabase
+
+_log = logging.getLogger(__name__)
+
+class Neo4jConnection(ConnectionInterface):
+    def __init__(self, connection_params, cim_profile:str):
+        self.cypher = importlib.import_module('cimgraph.loaders.sparql.' + cim_profile)
+        self.cim = importlib.import_module('cimgraph.data_profile.' + cim_profile)
+        self.connection_params = connection_params
+
+    def connect(self):
+        if not self.sparql_obj:
+            url = self.connection_params.parameters[0].value
+            self.sparql_obj = SPARQLWrapper(url)
+            self.sparql_obj.setReturnFormat(JSON)
