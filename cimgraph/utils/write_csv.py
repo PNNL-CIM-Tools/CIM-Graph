c00e5d55527d5a1fec828065d78350740bc909b4
aandersn <alexander.a.anderson@ieee.org>
2023-11-22 20:07:26 -0800

    update gitignore

--- cimgraph/utils/write_csv.py
+++ cimgraph/utils/write_csv.py
@@ -9,3 +9,12 @@ import uuid
 
 from cimgraph.models.graph_model import GraphModel, json_dump
 _log = logging.getLogger(__name__)
+
+def write_csv(network:GraphModel, destination) -> None:
+
+    for cim_class in list(network.graph.keys()):
+        attributes = list(cim_class.__dataclass_fields__.keys())
+
+        with open(f'{destination}/{cim_class.__name__}.csv', 'w') as csv_file:
+            header = ','.join(attributes)
+            csv_file.write(header)
