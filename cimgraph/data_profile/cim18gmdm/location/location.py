325c9a337ecac31955791c931b717ab727dc2a1a
aandersn <alexander.a.anderson@ieee.org>
2025-10-29 22:13:59 -0700

    missing crsUrn

--- cimgraph/data_profile/cim18gmdm/location/location.py
+++ cimgraph/data_profile/cim18gmdm/location/location.py
@@ -106,6 +106,43 @@ class CoordinateSystem(IdentifiedObject):
     All locations described with position points in this coordinate system.
     '''
     
+    crsUrn: Optional[str] = field(
+        default=None,
+        metadata={
+        'type': 'Attribute',
+        'minOccurs': '1',
+        'maxOccurs': '1',
+        'namespace': 'http://cim.ucaiug.io/CIM101/draft#',
+        'docstring':
+            '''
+            A Uniform Resource Name (URN) for the coordinate reference system (crs)
+            used to define 'Location.PositionPoints'.
+            An example would be the European Petroleum Survey Group (EPSG) code for
+            a coordinate reference system, defined in URN under the Open Geospatial
+            Consortium (OGC) namespace as: urn:ogc:def:crs:EPSG::XXXX, where XXXX is
+            an EPSG code (a full list of codes can be found at the EPSG Registry web
+            site http://www.epsg-registry.org/). To define the coordinate system as
+            being WGS84 (latitude, longitude) using an EPSG OGC, this attribute would
+            be urn:ogc:def:crs:EPSG::4236.
+            A profile should limit this code to a set of allowed URNs agreed to by
+            all sending and receiving parties.
+            '''
+        
+        })
+    '''
+    A Uniform Resource Name (URN) for the coordinate reference system (crs)
+    used to define 'Location.PositionPoints'.
+    An example would be the European Petroleum Survey Group (EPSG) code
+    for a coordinate reference system, defined in URN under the Open Geospatial
+    Consortium (OGC) namespace as: urn:ogc:def:crs:EPSG::XXXX, where XXXX
+    is an EPSG code (a full list of codes can be found at the EPSG Registry
+    web site http://www.epsg-registry.org/). To define the coordinate system
+    as being WGS84 (latitude, longitude) using an EPSG OGC, this attribute
+    would be urn:ogc:def:crs:EPSG::4236.
+    A profile should limit this code to a set of allowed URNs agreed to
+    by all sending and receiving parties.
+    '''
+    
 @stereotype(CIMStereotype.deprecated)
 @dataclass(repr=False)
 class ElectronicAddress(IdentifiedObject):
